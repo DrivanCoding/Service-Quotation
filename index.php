c08008d319dc8df4a424b3c24f179af8f72d18c2
Drivancoding <Drivancoding@gmail.com>
2022-06-21 17:56:27 +0200

    version 1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+plugin Name:service quotation
+Description: permet de ressortir la cotation des different client a 
+plugin URL:http://wordpress.com
+Author:ivan
+Author URL:http://wordpress.com
+*/
