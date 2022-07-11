f198e864c62e14f5a803de47300a176b176c75e2
Drivancoding <Drivancoding@gmail.com>
2022-07-11 15:22:08 +0200

    version 1.1.0

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'servicequotation' );
+define( 'DB_NAME', 'sql6505565' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'sql6505565' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Please wait' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql6.freemysqlhosting.net' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
